f278cf4a537aa7867bec54c83c6bd16eee51cad7
John <jaspajjr@gmail.com>
2017-08-13 22:37:46 +0100

    Had a go at updating the dictionary

--- main.py
+++ main.py
@@ -1,45 +1,25 @@
 import asyncio
-import os
-import urllib.request
 
 
-async def download_coroutine(url):
+async def add_item(state, key, value):
     '''
-    A coroutine to dl the specified url.
+    Add key, value to the state dictionary
     '''
-    req = urllib.request.urlopen(url)
-    file_name = os.path.basename(url)
-    print(file_name)
-
-    with open(file_name, 'wb') as file_handle:
-        while True:
-            chunk = req.read(1024)
-            if not chunk:
-                break
-            file_handle.write(chunk)
-        msg = 'Finished downloading {filename}'.format(filename=file_name)
-    return msg
-
-
-async def main(urls):
-        '''
-        Creates a group of coroutines and waits for them to finish.
-        '''
-        coroutines = [download_coroutine(url) for url in urls]
-        completed, pending = await asyncio.wait(coroutines)
-        for item in completed:
-            print(item.result())
+    def do_thing():
+        state[key] = value
+    await do_thing()
+    print(state)
 
 
 if __name__ == '__main__':
-    urls = ["http://www.irs.gov/pub/irs-pdf/f1040.pdf",
-            "http://www.irs.gov/pub/irs-pdf/f1040a.pdf",
-            "http://www.irs.gov/pub/irs-pdf/f1040ez.pdf",
-            "http://www.irs.gov/pub/irs-pdf/f1040es.pdf",
-            "http://www.irs.gov/pub/irs-pdf/f1040sb.pdf"]
+    state = {}
+    key_list = ['word'.join('_{}'.format(x)) for x in range(10)]
+    value_list = [x for x in range(10)]
+    tasks = [add_item(state, 1, 2)]
 
     event_loop = asyncio.get_event_loop()
     try:
-        event_loop.run_until_complete(main(urls))
+        event_loop.run_until_complete(
+                asyncio.async(add_item(state, 'hello', 2)))
     finally:
         event_loop.close()
